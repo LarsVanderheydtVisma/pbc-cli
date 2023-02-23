48c6e63f14939eda5c739e6c2de061bfcb2cbe0f
Lars Vanderheydt <lars.vanderheydt@visma.com>
2023-02-23 19:37:55 +0100

    methods used in the test should be declared before running the describe firstChild is needed because of the fixed div

--- bin/PBC/component/[name].test.tsx
+++ bin/PBC/component/[name].test.tsx
@@ -3,21 +3,6 @@ import ReactDOM from 'react-dom';
 import { assert } from 'chai';
 import { _name as name } from './[name].stories';
 
-describe('name', () => {
-    beforeEach(removeContainer);
-
-    it('should render [name]', () => {
-        mount(
-            <div id="[nameLowerCase]">
-                <name />
-            </div>
-        );
-
-        const nameLowerCase = document.getElementById('[nameLowerCase]');
-        assert.exists(nameLowerCase);
-    });
-});
-
 const mount = (name: ReactElement): { div: HTMLElement } => {
     const div = document.createElement('div');
     div.dataset.testid = 'container';
@@ -33,3 +18,18 @@ const mount = (name: ReactElement): { div: HTMLElement } => {
 const removeContainer = () => {
     document.querySelector('[data-testid="container"]')?.remove();
 };
+
+describe('name', () => {
+    beforeEach(removeContainer);
+
+    it('should render [name]', () => {
+        mount(
+            <div id="[nameLowerCase]">
+                <name />
+            </div>
+        );
+
+        const nameLowerCase = document.getElementById('[nameLowerCase]')?.firstChild;
+        assert.exists(nameLowerCase);
+    });
+});
